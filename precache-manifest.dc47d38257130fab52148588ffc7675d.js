e3502af232ddc3c9ce29552b790b5fc4bae3d1a0
Aurora28 <472645313@qq.com>
2020-07-01 17:24:34 -0700

    Updates

---
+++ precache-manifest.dc47d38257130fab52148588ffc7675d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f4c23511ca0a32c76c6a0715a3853b8",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "cefcc05f8aabd9bbdff9",
+    "url": "/monsters-rolodex/static/css/main.d1f996c4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cefcc05f8aabd9bbdff9",
+    "url": "/monsters-rolodex/static/js/main.5068af20.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
